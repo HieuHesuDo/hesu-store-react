3cce8df220bce8b6f8b676707d1124a6bd9e1f6f
binbommmm <dotrunghieu259@gmail.com>
2021-06-05 23:14:31 +0700

    Sửa currency stripe

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import StripeCheckout from "react-stripe-checkout";
 
 const StripeCheckoutButton = ({ price }) => {
-  const priceForStripe = price * 100;
+  const priceForStripe = price ;
   const publishableKey =
     "pk_test_51IySeoLriCNTqOPiRlnxh8W2B9s4pxLglfICiJN2fntiRASCdd1hfOFNW949bZUMU1BVyOehSY1L4uE9G4KgbM1800Ziw0EFem";
   const onToken = (token) => {
@@ -18,6 +18,7 @@ const StripeCheckoutButton = ({ price }) => {
       description={`Tổng tiền ${price.toLocaleString("it-IT", { style: "currency", currency: "VND" })}`}
       amount={priceForStripe}
       panelLabel="Thanh toán"
+      currency='vnd'
       token={onToken}
       stripeKey={publishableKey}
       image='https://cdn.thukyluat.vn/nhch-images//CauHoi_Hinh/9eb6abaa-8cda-456c-ad66-26ba4da23ffe.jpg'
